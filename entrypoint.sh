34700b24a81c329001a1799fe576e0f76406ddfa
Dustin Ward <dustin.n.ward@gmail.com>
2021-07-30 13:12:26 -0500

    update entryfile.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ set -o pipefail
 
 echo "Running ESigner.com CodeSign Action ====>"
 echo ""
+echo ${INPUT_COMMAND}
 
 COMMAND="java -cp '.:/codesign/jar/*' com.ssl.code.signing.tool.CodeSignTool ${INPUT_COMMAND}"
+echo $COMMAND
 
 sh -c "set -e;  set -o pipefail; $COMMAND"
 

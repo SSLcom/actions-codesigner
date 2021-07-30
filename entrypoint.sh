f6e10f0ca7eb81654187970eafe4f01c3e63ffb9
Dustin Ward <dustin.n.ward@gmail.com>
2021-07-30 12:28:11 -0500

    add workflow testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 set -e
 set -o pipefail
 
-COMMAND="java -cp '.:/codesign/jar/*' com.ssl.code.signing.tool.CodeSignTool ${@}"
+echo "RUNNING ACTION ====>"
+
+COMMAND="java -cp '.:/codesign/jar/*' com.ssl.code.signing.tool.CodeSignTool $@"
+echo $COMMAND
 
 bash -c "set -e;  set -o pipefail; $COMMAND"

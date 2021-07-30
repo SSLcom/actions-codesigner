05926da70e29560dcecb35283a3d296cddaa720a
Dustin Ward <dustin.n.ward@gmail.com>
2021-07-30 14:24:06 -0500

    update entryfile.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ set -o pipefail
 echo "Running ESigner.com CodeSign Action ====>"
 echo ""
 
-COMMAND="cd codesign; java -cp '.:/codesign/jar/*' com.ssl.code.signing.tool.CodeSignTool"
+COMMAND="cd /codesign; java -cp '.:/codesign/jar/*' com.ssl.code.signing.tool.CodeSignTool"
 
 [ ! -z $INPUT_COMMAND ] && COMMAND="$COMMAND $INPUT_COMMAND"
 [ ! -z $INPUT_USERNAME ] && COMMAND="$COMMAND -username $INPUT_USERNAME"
 [ ! -z $INPUT_PASSWORD ] && COMMAND="$COMMAND -password $INPUT_PASSWORD"
-[ ! -z "$INPUT_CREDENTIAL_ID" ] && COMMAND="${COMMAND} -credential_id ${INPUT_CREDENTIAL_ID}"
-[ ! -z "$INPUT_TOTP_SECRET" ] && COMMAND="${COMMAND} -totp_secret ${INPUT_TOTP_SECRET}"
-[ ! -z "$INPUT_PROGRAM_NAME" ] && COMMAND="${COMMAND} -program_name ${INPUT_PROGRAM_NAME}"
-[ ! -z "$INPUT_INPUT_FILE" ] && COMMAND="${COMMAND} -input_dir_path ${INPUT_INPUT_FILE}"
-[ ! -z "$INPUT_OUTPUT_DIR" ] && COMMAND="${COMMAND} -output_dir_path ${INPUT_OUTPUT_DIR}"
+[ ! -z $INPUT_CREDENTIAL_ID ] && COMMAND="${COMMAND} -credential_id ${INPUT_CREDENTIAL_ID}"
+[ ! -z $INPUT_TOTP_SECRET ] && COMMAND="${COMMAND} -totp_secret ${INPUT_TOTP_SECRET}"
+[ ! -z $INPUT_PROGRAM_NAME ] && COMMAND="${COMMAND} -program_name ${INPUT_PROGRAM_NAME}"
+[ ! -z $INPUT_INPUT_FILE ] && COMMAND="${COMMAND} -input_dir_path ${INPUT_INPUT_FILE}"
+[ ! -z $INPUT_OUTPUT_DIR ] && COMMAND="${COMMAND} -output_dir_path ${INPUT_OUTPUT_DIR}"
 
 echo $COMMAND
 

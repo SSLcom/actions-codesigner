a8962daf4ad6a5bddf34282c96073a1539a78b7a
Mustafa Bayrak <mustafa.bayrak@windowslive.com>
2023-07-25 20:33:26 +0300

    Added batch signing support

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,18 +20,26 @@ COMMAND="/usr/bin/codesign"
 [ ! -z $INPUT_TOTP_SECRET ] && COMMAND="${COMMAND} -totp_secret ${INPUT_TOTP_SECRET}"
 [ ! -z $INPUT_PROGRAM_NAME ] && COMMAND="${COMMAND} -program_name ${INPUT_PROGRAM_NAME}"
 [ ! -z $INPUT_FILE_PATH ] && COMMAND="${COMMAND} -input_file_path ${INPUT_FILE_PATH}"
+[ ! -z $INPUT_DIR_PATH ] && COMMAND="${COMMAND} -input_dir_path ${INPUT_DIR_PATH}"
 [ ! -z $INPUT_OUTPUT_PATH ] && COMMAND="${COMMAND} -output_dir_path ${INPUT_OUTPUT_PATH}"
 [ ! -z $INPUT_MALWARE_BLOCK ] && COMMAND="${COMMAND} -malware_block=${INPUT_MALWARE_BLOCK}"
+[ ! -z $INPUT_OVERRIDE ] && COMMAND="${COMMAND} -override=${INPUT_OVERRIDE}"
 
 RESULT=$(bash -c "set -e; $COMMAND 2>&1")
 
-if [[ "$RESULT" =~ .*"Error".* || "$RESULT" =~ .*"Exception".* || "$RESULT" =~ .*"Missing required option".* ]]; then
+if [[ "$RESULT" =~ .*"Error".* || "$RESULT" =~ .*"Exception".* || "$RESULT" =~ .*"Missing required option".* ||
+      "$RESULT" =~ .*"Unmatched arguments from".* ]]; then
   echo "::error::Something Went Wrong. Please try again."
   echo "::error::$RESULT"
   exit 1
 else
   echo "SELECTED_COLOR=green" >> $GITHUB_OUTPUT
   echo "$RESULT"
+fi
+
+if [[ "$INPUT_CLEAN_LOGS" == "true" || "$INPUT_CLEAN_LOGS" == true ]]; then
+    rm -rf /codesign/logs/*.log
+    echo "CodeSigner logs folder is deleted: /codesign/logs"
 fi
 
 echo "::endgroup::"
